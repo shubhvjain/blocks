b9e81a140f45d8e8d6916cddc7a7919d525f44d9
shubh <shubhvjain@gmail.com>
2022-10-24 07:15:34 +0200

    created develop script to ease development, added list of useful scripts in readme

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # Blocks
-Text markup and document processing system. The purpose is to separate the text and display style so that a single text source can be displayed in multiple formats. 
 
-For example, the same text can be used to generate a pdf file (in LaTEX), web page (in HTML), a slide deck (html/pdf), plain text, mind map and many more.  
+[Documentation](https://shubhvjain.github.io/blocks/)
+
+Text markup and document processing system. 
+
+The purpose is to separate the text and display style so that a single text source can be displayed in multiple formats. For example, the same text can be used to generate a pdf file (in LaTEX), web page (in HTML), a slide deck (html/pdf), plain text, mind map and many more.  
 
 Another aim is to support [literate programming](https://en.wikipedia.org/wiki/Literate_programming). Keeping the code alongside it's explanation for humans to understand the code better and generating a separate code file for machines.
 
 A document (the input the the program) consists  of blocks, the basic processing unit.
-Each block is separated by a new line. This means a block can be a single sentence,a paragraph, a list, a heading, code etc. 
-Metadata related to a block is defined within the annotation delimiter. 
+Each block is separated by a new line. This means a block can be a single sentence,a paragraph, a list, a heading, code etc.  Metadata related to a block is defined within the annotation delimiter. 
 
-
-# Dev notes
 
 ## folder structure 
 there  is a single level folder hierarchy i.e no sub folders. all the code including related code remains in the same folder. There are, however, 4  folders in the root folder of the project.
@@ -24,3 +24,11 @@ there  is a single level folder hierarchy i.e no sub folders. all the code inclu
 run this command from the base directory of the project : `node ./script/release.js versionType`
 
 (valid version types : `major` , `minor` , `patch`)
+
+## Useful scripts 
+
+Run from the root of the project
+
+- `node test/develop.js cmd` : to run testing commands while working on the block program. like generating a new copy of block.js for testing using the lastest released version of the program. use '0' as 'cmd' for the list of all commands
+- `cp plugins/vscode-syntax-highlight/syntaxes/block.tmLanguage.json ~/.vscode/extensions/vscode-syntax-hilighter/syntaxes ` : to update the syntax file for vscode highlight extension on mac. Assuming the folder is already copied once
+- `node scripts/release.js versionType` : to release a new version of the program. specify version type (`major`, `minor` or `patch`)
---
+++ test/develop.js
@@ -0,0 +1,66 @@
+// use this while working on the block code. 
+// run this from the root dir of the project
+const fs = require("fs/promises");
+
+const saveFile = async (filePath, fileContent) => {await fs.writeFile(filePath,fileContent)}
+const getFile = async (filePath) => {const data = await fs.readFile(filePath, { encoding: "utf8" });return data;}
+
+const blockLatest = require("../release/block")
+const blockTest = require("../test/block")
+
+// this will generate the latest test version of block.js (inside the test folder) using the latest released version of the block program
+const generateTestBlockCodeUsingLatestRelease = async ()=>{
+  let fileContent = await getFile("source/block.txt")
+  // todo change generateDocument to  generateOutputDoc after new release
+  const processedDoc  = blockLatest.generateDocument(fileContent,{main:"program-for-testing"})
+  await saveFile("test/block.js",processedDoc)
+  console.log("test/block.js updated!")
+}
+
+// this generates the explorer document for the lastest source file (i.e. the block.txt inside the source folder)
+// this will use the block program in the test folder 
+const generateExplorerDocForSourceUsingTest = async ()=>{
+  let fileContent = await getFile("source/block.txt")
+  let generatedDoc = await blockTest.generateOutputDoc(fileContent,{type:"explorer"})
+  await saveFile("test/explorer-block.html",generatedDoc)
+}
+
+const generateExplorerDocForDocUsingTest = async (docPath)=>{
+  let fileContent = await getFile(docPath)
+  const parts = docPath.split("/")
+  const nameparts = parts[parts.length-1].split(".")
+  const name = nameparts[0]
+  let generatedDoc = await blockTest.generateOutputDoc(fileContent,{type:"explorer"})
+  await saveFile(`test/explorer-${name}.html`,generatedDoc)
+  console.log(`generated : test/explorer-${name}.html`)
+}
+
+const main = async ()=>{
+  const cmdName = process.argv[2]
+  cmds = {
+    0 : async ()=>{
+      const help =  ` Hello! Here is the list of commands : 
+0: help
+1: generate test/block.js using release/block.js
+2: generate explorer doc for source/block.txt using release/block.js
+3: generate explorer doc for source/block.txt using test/block.js
+4: generate explorer doc for specified input file (saved in the test folder)
+`
+      console.log(help)
+    },
+    1: async () =>{
+      await generateTestBlockCodeUsingLatestRelease()
+    },
+    2: async()=>{ console.log("....after next version is released")},
+    3: async () =>{
+      await generateExplorerDocForSourceUsingTest()
+    },
+    4: async ()=>{
+
+    }
+  }
+  if(!cmds[cmdName]){throw new Error("invalid command. use '0' for list of available commands")}
+  await cmds[cmdName]()
+}
+
+main().then(d=>{console.log("Done!")}).catch(e=>{console.log(e)})
